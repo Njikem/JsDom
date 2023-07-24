5ae0888e89ecde9d5607500ea1b21075297cc1f5
Njikem <123434988+Njikem@users.noreply.github.com>
2023-07-24 00:07:42 -0600

    dom

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let first_p_tag = document.querySelector('p');
+
+let first_id = document.querySelector('id');
+
+let all_class_tag = document.querySelectorAll('p');
+
+let p_tags = document.getElementById('title');
+
+let best_p_tags = document.getElementsByClassName('content');
